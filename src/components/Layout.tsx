87b1ccac2341cbbca2ff45d3920f4418caeea9b3
ersetdigital-sudo <ersetdigital@gmail.com>
2026-06-11 02:11:28 +0700

    auto: update 2026-06-11 02:11:28

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@ import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Toaster from './Toaster';
+import CartDrawer from './CartDrawer';
 
 export default function Layout() {
   const { pathname } = useLocation();
